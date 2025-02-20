443c26bb837c112d349c24d234f4b52cc73d69c2
yj901 <901duwls@gmail.com>
2025-02-20 19:24:33 +0900

    header-style-ing

--- common.js
+++ common.js
@@ -1,7 +1,6 @@
 // headaer
 const header = document.querySelector("#header");
 const gnb = document.querySelector("#gnb");
-const header_left = document.querySelector(".header_left");
 const m_trigger_bar = document.querySelector(".m_trigger_bar");
 const m_header_menu = document.querySelector("#m_header_menu");
 
@@ -9,42 +8,18 @@ m_trigger_bar.addEventListener("click", function () {
   this.classList.toggle("active");
   if (this.classList.contains("active")) {
     m_header_menu.classList.add("active");
+    gnb.classList.add("active");
   } else {
     m_header_menu.classList.remove("active");
+    gnb.classList.remove("active");
   }
 });
 
-window.addEventListener("resize", () => {
-  if (window.matchMedia("(max-width: 1023px)").matches) {
-    header.classList.add("menu_mobile");
+window.addEventListener("scroll", () => {
+  console.log("teset");
+  if (window.scrollY > 50) {
+    header.classList.add("scroll");
   } else {
-    header.classList.remove("menu_mobile");
+    header.classList.remove("scroll");
   }
-
-  // if (header.classList.contains("menu_mobile")) {
-  //   // header_left.removeChild(gnb);
-  //   // m_header_menu.appendChild(gnb);
-  //   console.log("test1");
-  // } else {
-  //   // m_header_menu.appendChild(gnb);
-  //   // header_left.removeChild(gnb);
-  //   console.log("test2");
-  // }
 });
-
-// 딱 한번만 체크해야하는데....
-if (header.classList.contains("menu_mobile")) {
-  // header_left.removeChild(gnb);
-  // m_header_menu.appendChild(gnb);
-  console.log("test1");
-} else {
-  // m_header_menu.appendChild(gnb);
-  // header_left.removeChild(gnb);
-  console.log("test2");
-}
-
-// if (header.classList.contains("menu_mobile") !== true) {
-//   // m_header_menu.appendChild(gnb);
-//   // header_left.removeChild(gnb);
-//   console.log("test2");
-// }
