7801abd2d2c41aa017fbeba4061322a2336cb65a
yj901 <901duwls@gmail.com>
2025-02-20 21:26:40 +0900

    header complete commit

--- common.js
+++ common.js
@@ -1,9 +1,11 @@
-// headaer
+// header 공통 JS
+let lastScrollTop = 0;
 const header = document.querySelector("#header");
 const gnb = document.querySelector("#gnb");
 const m_trigger_bar = document.querySelector(".m_trigger_bar");
 const m_header_menu = document.querySelector("#m_header_menu");
 
+// 모바일 gnb 사이드바 메뉴 토글
 m_trigger_bar.addEventListener("click", function () {
   this.classList.toggle("active");
   if (this.classList.contains("active")) {
@@ -15,11 +17,26 @@ m_trigger_bar.addEventListener("click", function () {
   }
 });
 
+// header 스크롤 체크, 히든
 window.addEventListener("scroll", () => {
-  console.log("teset");
+  let scrollTop = window.scrollY;
+
+  if (scrollTop > lastScrollTop) {
+    header.classList.add("hidden");
+  } else {
+    header.classList.remove("hidden");
+  }
+
+  lastScrollTop = scrollTop >= 0 ? scrollTop : 0;
+
   if (window.scrollY > 50) {
     header.classList.add("scroll");
   } else {
     header.classList.remove("scroll");
+  }
+
+  // 사이드 메뉴 바 열였을 때는 hidden 막기
+  if (m_trigger_bar.classList.contains("active")) {
+    header.classList.remove("hidden");
   }
 });
