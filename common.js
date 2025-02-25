b4f80d7ee86dec02a62d3f8c956239a7cfb3bb42
Hyo-A <janggydk@daum.net>
2025-02-25 20:42:59 +0900

    0225detail

--- common.js
+++ common.js
@@ -2,17 +2,22 @@
 let lastScrollTop = 0;
 const header = document.querySelector("#header");
 const gnb = document.querySelector("#gnb");
-const m_trigger_bar = document.querySelector(".m_trigger_bar");
-const m_header_menu = document.querySelector("#m_header_menu");
+const header_menu_m = document.querySelector("#header_menu_m");
+const trigger_bar_m = document.querySelector(".trigger_bar_m");
+const search_btn_m = document.querySelector(".search_btn_m");
+const search_area_m = document.querySelector("#search_area_m");
+const search_close_m = search_area_m.querySelector(".search_close_m");
+const search_form_pc = document.querySelector("#search_form_pc");
+const search_form_mb = document.querySelector("#search_form_mb");
 
 // 모바일 gnb 사이드바 메뉴 토글
-m_trigger_bar.addEventListener("click", function () {
+trigger_bar_m.addEventListener("click", function () {
   this.classList.toggle("active");
   if (this.classList.contains("active")) {
-    m_header_menu.classList.add("active");
+    header_menu_m.classList.add("active");
     gnb.classList.add("active");
   } else {
-    m_header_menu.classList.remove("active");
+    header_menu_m.classList.remove("active");
     gnb.classList.remove("active");
   }
 });
@@ -36,7 +41,46 @@ window.addEventListener("scroll", () => {
   }
 
   // 사이드 메뉴 바 열었을 때는 hidden 막기
-  if (m_trigger_bar.classList.contains("active")) {
+  if (trigger_bar_m.classList.contains("active")) {
     header.classList.remove("hidden");
   }
+});
+
+// 모바일 검색창 열고닫기
+search_btn_m.addEventListener("click", () => {
+  search_area_m.classList.add("active");
+});
+search_close_m.addEventListener("click", () => {
+  search_area_m.classList.remove("active");
+});
+
+// 헤더 검색 이벤트 (서버제출x)
+const header_search = (e) => {
+  e.preventDefault();
+};
+search_form_pc.addEventListener("submit", header_search);
+search_form_mb.addEventListener("submit", header_search);
+
+// footer 이벤트
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 50) {
+    document.querySelector(".up_btn").classList.add("active");
+  } else {
+    document.querySelector(".up_btn").classList.remove("active");
+  }
+});
+
+// footer topscroll event
+const up_btn = document.querySelector(".up_btn > a");
+
+up_btn.addEventListener("click", function (e) {
+  e.preventDefault();
+  const targetId = this.getAttribute("href");
+  const targetElement = document.querySelector(targetId);
+  const targetPosition = targetElement.offsetTop;
+
+  window.scrollTo({
+    top: targetPosition,
+    behavior: "smooth",
+  });
 });
