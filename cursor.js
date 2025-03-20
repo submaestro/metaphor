25d0f123fa16577f559a0b4b5370cdf8fd7d1dc2
정휘찬 <jeonghwichan@jeonghwichan-ui-MacBookPro.local>
2025-03-20 20:15:22 +0900

    magazinesub

--- cursor.js
+++ cursor.js
@@ -1,23 +1,42 @@
 // Cursor Event
-let cursor = document.querySelector(".cursor")
+const cursor = document.querySelector(".cursor")
 console.log(cursor);
 
 // const cursor = document.getElementById('cursor');
 
 document.addEventListener('mousemove', (e) => {
-  console.log(e);
   cursor.style.left = `${e.pageX}px`;
   cursor.style.top = `${e.pageY}px`;
 });
 console.log(cursor);
 
 // 커서 영역에 마우스를 올리면 커서 색상 변경
-const cursorArea = document.querySelector('.cursor-area');
-console.log(cursorArea);
+// const test=document.querySelector('.test');
+// test.addEventListener("mouseenter",()=>{
+//   console.log("test");
 
-cursorArea.addEventListener('mouseover', function() {
-  cursor.style.backgroundColor = 'lightblue';  // 커서 색상 변경 (초록색)
-});
-cursorArea.addEventListener('mouseout', function() {
-  cursor.style.backgroundColor = 'none';  // 원래 커서 색상으로 복원 (빨간색)
-});
+
+  
+// })
+const pointers = document.querySelectorAll('.artist_icons li a');
+
+pointers.forEach((pointer) => {
+
+  pointer.addEventListener('mouseover', ()=> {
+    cursor.style.backgroundColor = '#03d8c5';  // 커서 색상 변경 (초록색)
+  
+  });
+  pointer.addEventListener('mouseout', function() {
+    cursor.style = 'none';
+  });
+})
+
+
+const li = document.querySelectorAll(".artist_icons li");
+console.log(li);
+
+li.forEach(item => {
+  
+})
+
+
